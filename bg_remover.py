00aa560db72f6e4aa7fefa98fc565cf647b13731
jabet <collegejcastersen@gmail.com>
2024-08-31 19:33:24 -0700

    onnx support

--- bg_remover.py
+++ bg_remover.py
@@ -1,3 +1,5 @@
+from typing import List
+from pathlib import Path
 from transformers import AutoModelForImageSegmentation
 import torch
 import torch.nn.functional as F
@@ -5,39 +7,72 @@ from torchvision.transforms.functional import normalize
 import numpy as np
 from PIL import Image
 import io
+import requests
+import onnxruntime as ort
 
-class ImageProcessor:
+ONNX_CACHE_DIR = 'onnx_cache'
+
+def load_onnx_model(repo_name: str, cache_dir: str = ONNX_CACHE_DIR) -> ort.InferenceSession:
+    model_filename = f"{repo_name.replace('/', '_')}_quantized.onnx"
+    cached_model_path = Path(cache_dir) / model_filename
+
+    if not cached_model_path.exists():
+        model_url = f'https://huggingface.co/{repo_name}/resolve/main/onnx/model_quantized.onnx'
+        print(f'Downloading model from {model_url}')
+        response = requests.get(model_url)
+        response.raise_for_status()
+        cached_model_path.write_bytes(response.content)
+    
+    return ort.InferenceSession(str(cached_model_path))
+
+def preprocess_image(im: np.ndarray, model_input_size: list) -> torch.Tensor:
+    im_tensor = torch.tensor(im, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
+    im_tensor = F.interpolate(im_tensor, size=model_input_size, mode='bilinear')
+    return normalize(im_tensor / 255.0, [0.5, 0.5, 0.5], [1.0, 1.0, 1.0])
+
+def postprocess_image(result: torch.Tensor, im_size: list)-> np.ndarray:
+    result = F.interpolate(result, size=im_size, mode='bilinear').squeeze(0)
+    return (result.clamp(0,1) * 255).byte().permute(1,2,0).cpu().numpy().squeeze()
+
+class BaseImageProcessor:
+    def do_inference(self, image: np.ndarray, image_size: List[int], model_size: List[int]):
+        pass
+
+class ONNXImageProcessor(BaseImageProcessor):
     def __init__(self):
-        print('Loading model this might take a while...')
+        print('Loading quantized onnx model')
+        self.model = load_onnx_model('briaai/RMBG-1.4')
+
+    def do_inference(self, image, image_size, model_size):
+        preprocessed_image = preprocess_image(image, model_size)
+        results = self.model.run(['output'], {'input': preprocessed_image.numpy()})[0]
+        return postprocess_image(torch.Tensor(results), image_size)
+
+class TorchImageProcessor(BaseImageProcessor):
+    def __init__(self):
+        print('Loading model, this might take a while...')
         self.model = AutoModelForImageSegmentation.from_pretrained("briaai/RMBG-1.4",trust_remote_code=True)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         print(f'Using {self.device}')
         self.model.to(self.device)
         self.model.eval()
 
-    def preprocess_image(self, im: np.ndarray, model_input_size: list) -> torch.Tensor:
-        im_tensor = torch.tensor(im, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
-        im_tensor = F.interpolate(im_tensor, size=model_input_size, mode='bilinear')
-        return normalize(im_tensor / 255.0, [0.5] * 3, [1.0] * 3)
+    def do_inference(self, image, image_size, model_size):
+        preprocessed_image = preprocess_image(image, model_size).to(self.device)
+        with torch.no_grad():
+            results = self.model(preprocessed_image)
+        return postprocess_image(results[0][0], image_size)
 
-    def postprocess_image(self, result: torch.Tensor, im_size: list)-> np.ndarray:
-        result = F.interpolate(result, size=im_size, mode='bilinear').squeeze(0)
-        result = (result - result.min()) / (result.max()-result.min())
-        return (result * 255).byte().permute(1,2,0).cpu().numpy().squeeze()
+def run_model(image_bytes: bytes, processor: BaseImageProcessor):
+    with Image.open(io.BytesIO(image_bytes)) as image:
+        if image.mode == 'RGBA':
+            image = image.convert('RGB')
 
-    def get_image_bytes(self, image_bytes: bytes) -> bytes:
-        with Image.open(io.BytesIO(image_bytes)) as image:
-            processed_image = self.preprocess_image(np.array(image), [1024,1024]).to(self.device)
+        result_array = processor.do_inference(np.array(image), image.size[::-1], [1024,1024]) 
+        result_image = Image.fromarray(result_array)
+        transparent_image = Image.new('RGBA', result_image.size, (0,0,0,0))
+        transparent_image.paste(image, mask=result_image)
+        output_image = io.BytesIO()
+        transparent_image.save(output_image, format="PNG", compress_level=1)
 
-            with torch.no_grad():
-                results = self.model(processed_image)
-
-            result_array = self.postprocess_image(results[0][0], image.size[::-1])
-            result_image = Image.fromarray(result_array)
-
-            transparent_image = Image.new('RGBA', result_image.size, (0,0,0,0))
-            transparent_image.paste(image, mask=result_image)
-            output_image = io.BytesIO()
-            transparent_image.save(output_image, format="PNG", compress_level=1)
-
-        return output_image.getvalue()
+    return output_image.getvalue()
--- server.py
+++ server.py
@@ -6,13 +6,16 @@ from functools import lru_cache
 from pathlib import Path
 import json
 import base64
-from bg_remover import ImageProcessor
+from bg_remover import BaseImageProcessor, ONNXImageProcessor, TorchImageProcessor, run_model
 
-PROCESSOR = ImageProcessor()
 START_PAGE = 'index.html'
 
-class ManPageHandler(SimpleHTTPRequestHandler):
+class BGRemoverHandler(SimpleHTTPRequestHandler):
     protocol_version = 'HTTP/1.1'
+
+    def __init__(self, *args, processor: BaseImageProcessor=None, **kwargs) -> None:
+        self.processor = processor
+        super().__init__(*args, **kwargs)
 
     def __send_200_headers(self, length: int, type='text/html; charset=UTF-8'):
         self.send_response(HTTPStatus.OK)
@@ -20,7 +23,7 @@ class ManPageHandler(SimpleHTTPRequestHandler):
         self.send_header('Content-Length', str(length))
         self.end_headers()
 
-    # @lru_cache(maxsize=32)
+    @lru_cache(maxsize=32)
     def __get_page(self, page) -> bytes:
         with open(page, 'r') as fr:
             return fr.read().encode('utf-8')
@@ -34,41 +37,33 @@ class ManPageHandler(SimpleHTTPRequestHandler):
         if self.path == '/favicon.ico':
             self.send_response(HTTPStatus.NOT_FOUND)
             self.end_headers()
-        elif self.path.endswith('.css') or self.path.endswith('.js'):
-            self.__send_page(Path(self.path).name)
         else:
-            self.__send_page(START_PAGE)
+            file_path = Path(self.path).name or Path(START_PAGE)
+            self.__send_page(file_path)
 
     def do_POST(self):
         data = json.loads(self.rfile.read(int(self.headers['Content-Length'])))
-        image_bytes = PROCESSOR.get_image_bytes(base64.b64decode(data['file']))
+        image_bytes = run_model(base64.b64decode(data['file']), self.processor)
         self.__send_200_headers(len(image_bytes), type='image/png')
         self.wfile.write(image_bytes)
 
 def main():
-    port = 8001
-    host = 'localhost'
-    allow_reuse = False
-
     parser = argparse.ArgumentParser(description='Remove BG Options')
-    parser.add_argument('-p', '--port', type=int, help='Set port')
-    parser.add_argument('-i', '--host', type=str, help='Set host')
-    parser.add_argument('-a', '--allow', action='store_true', help='Allow reuse of address')
+    parser.add_argument('-p', '--port', type=int, default=8001, help='Set port')
+    parser.add_argument('-i', '--host', type=str, default='localhost', help='Set host')
+    parser.add_argument('-a', '--allow-reuse', action='store_true', help='Allow reuse of address')
+    parser.add_argument('-o', '--onnx', action='store_true', help='Run model using on cpu using onnx runtime')
 
     args = parser.parse_args()
-    if (args.port):
-        port = args.port
-    if (args.host):
-        host = args.host
-    if (args.allow):
-        allow_reuse = args.allow
+    processor = ONNXImageProcessor() if args.onnx else TorchImageProcessor()
 
     class ThreadingTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
         daemon_threads = True
-        allow_reuse_address = allow_reuse
+        allow_reuse_address = args.allow_reuse
 
-    print(f'Starting server at {host} port {port}')
-    with ThreadingTCPServer((host, port), ManPageHandler) as server:
+    print(f'Starting server at {args.host} port {args.port}')
+    with ThreadingTCPServer((args.host, args.port), 
+                            lambda *a, **kw: BGRemoverHandler(*a, processor=processor, **kw)) as server:
         server.serve_forever()
 
 if __name__ == '__main__':
