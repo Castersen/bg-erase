ff1590a6d9df29e0cb5f8a1aac2ebbc5df3cfe0f
jabet <collegejcastersen@gmail.com>
2024-12-10 14:07:55 -0800

    rmbg2.0 support

--- bg_remover.py
+++ bg_remover.py
@@ -1,16 +1,20 @@
-from typing import List
 from pathlib import Path
 from transformers import AutoModelForImageSegmentation
+from torchvision import transforms
 import torch
-import torch.nn.functional as F
-from torchvision.transforms.functional import normalize
-import numpy as np
-from PIL import Image
+from PIL import Image, ImageFile
 import io
 import requests
 import onnxruntime as ort
 
 ONNX_CACHE_DIR = 'onnx_cache'
+MODEL = 'briaai/RMBG-2.0'
+
+transform_image = transforms.Compose([
+    transforms.Resize((1024, 1024)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
 
 def load_onnx_model(repo_name: str, quantized: bool, cache_dir: str = ONNX_CACHE_DIR) -> ort.InferenceSession:
     model_filename = f"{repo_name.replace('/', '_')}_{'quantized' if quantized else 'model'}.onnx"
@@ -26,57 +30,47 @@ def load_onnx_model(repo_name: str, quantized: bool, cache_dir: str = ONNX_CACHE
     
     return ort.InferenceSession(str(cached_model_path))
 
-def preprocess_image(im: np.ndarray, model_input_size: list) -> torch.Tensor:
-    if len(im.shape) < 3:
-        im = im[:, :, np.newaxis]
-
-    im_tensor = torch.tensor(im, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
-    im_tensor = F.interpolate(im_tensor, size=model_input_size, mode='bilinear')
-    return normalize(im_tensor / 255.0, [0.5, 0.5, 0.5], [1.0, 1.0, 1.0])
-
-def postprocess_image(result: torch.Tensor, im_size: list)-> np.ndarray:
-    result = F.interpolate(result, size=im_size, mode='bilinear').squeeze(0)
-    return (result.clamp(0,1) * 255).byte().permute(1,2,0).cpu().numpy().squeeze()
+def preprocess_image(im: ImageFile) -> torch.Tensor:
+    return transform_image(im).unsqueeze(0)
 
 class BaseImageProcessor:
-    def do_inference(self, image: np.ndarray, image_size: List[int], model_size: List[int]):
+    def do_inference(self, image: ImageFile):
         pass
 
 class ONNXImageProcessor(BaseImageProcessor):
     def __init__(self, quantized: bool):
         print(f"Loading {'quantized' if quantized else ''} onnx model")
-        self.model = load_onnx_model('briaai/RMBG-1.4', quantized)
+        self.model = load_onnx_model(MODEL, quantized)
 
-    def do_inference(self, image, image_size, model_size):
-        preprocessed_image = preprocess_image(image, model_size)
-        results = self.model.run(['output'], {'input': preprocessed_image.numpy()})[0]
-        return postprocess_image(torch.Tensor(results), image_size)
+    def do_inference(self, image):
+        preprocessed_image = preprocess_image(image).numpy()
+        results = self.model.run(['alphas'], {'pixel_values': preprocessed_image})
+        return results[0].squeeze()
 
 class TorchImageProcessor(BaseImageProcessor):
     def __init__(self):
         print('Loading model, this might take a while...')
-        self.model = AutoModelForImageSegmentation.from_pretrained("briaai/RMBG-1.4",trust_remote_code=True)
+        self.model = AutoModelForImageSegmentation.from_pretrained(MODEL,trust_remote_code=True)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         print(f'Using {self.device}')
+        torch.set_float32_matmul_precision(['high', 'highest'][0])
         self.model.to(self.device)
         self.model.eval()
 
-    def do_inference(self, image, image_size, model_size):
-        preprocessed_image = preprocess_image(image, model_size).to(self.device)
+    def do_inference(self, image):
+        preprocessed_image = preprocess_image(image).to(self.device)
         with torch.no_grad():
-            results = self.model(preprocessed_image)
-        return postprocess_image(results[0][0], image_size)
+            results = self.model(preprocessed_image)[-1].sigmoid().cpu()
+        return results[0].squeeze()
 
 def run_model(image_bytes: bytes, processor: BaseImageProcessor):
     with Image.open(io.BytesIO(image_bytes)) as image:
         if image.mode == 'RGBA':
             image = image.convert('RGB')
 
-        result_array = processor.do_inference(np.array(image), image.size[::-1], [1024,1024]) 
-        result_image = Image.fromarray(result_array)
-        transparent_image = Image.new('RGBA', result_image.size, (0,0,0,0))
-        transparent_image.paste(image, mask=result_image)
+        result_array = processor.do_inference(image)
+        image.putalpha((transforms.ToPILImage()(result_array)).resize(image.size))
         output_image = io.BytesIO()
-        transparent_image.save(output_image, format="PNG", compress_level=1)
+        image.save(output_image, format="PNG", compress_level=1)
 
     return output_image.getvalue()
